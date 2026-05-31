25628710afdbfc5c6b188b6ae07993ee2f910327
mktvektor-ctrl <mktvektor@gmail.com>
2026-05-31 18:05:45 +0200

    fix port binding

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`amaIA corriendo en http://localhost:${PORT}`));
